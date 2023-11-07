923da988634cea6e01e462923142867c29629a82
Apexshya <gyawaliapeksha123@gmail.com>
2023-11-07 16:56:26 +0545

    made changes on index

--- public/api.js
+++ public/api.js
@@ -1,5 +1,32 @@
 
 
+// $(function() {
+//     $("#location").autocomplete({
+//       source: function(request, response) {
+//         $.ajax({
+//           url: 'https://nominatim.openstreetmap.org/search',
+//           dataType: 'json',
+//           data: {
+//             q: request.term,
+//             format: 'json',
+//             addressdetails: 1
+//           },
+//           success: function(data) {
+//             var locations = data.map(function(item) {
+//               return item.display_name;
+//             });
+  
+//             response(locations);
+//           },
+//           error: function() {
+//             // Handle errors or empty responses
+//           }
+//         });
+//       },
+//       minLength: 1
+//     });
+//   });
+
 $(function() {
     $("#location").autocomplete({
       source: function(request, response) {
@@ -23,9 +50,20 @@ $(function() {
           }
         });
       },
-      minLength: 1
+      minLength: 1,
+      select: function(event, ui) {
+        // Set the selected value in the input field
+        $("#location").val(ui.item.value);
+        return false; // Prevent the widget from updating the input field
+      }
     });
   });
+  
+
+
+
+
+
   
 
 
@@ -61,4 +99,3 @@ $(function() {
   });
   
 
-  
